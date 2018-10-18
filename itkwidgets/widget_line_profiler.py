7c12f8c94a829abe428e8928818c834b6c70cb6a
Matt McCormick <matt.mccormick@kitware.com>
2018-10-18 00:09:29 -0400

    ENH: Enable Python specification of line profile points

--- itkwidgets/widget_line_profiler.py
+++ itkwidgets/widget_line_profiler.py
@@ -5,8 +5,14 @@ Image visualization with a line profile.
 
 from traitlets import Unicode
 
+import numpy as np
 import ipywidgets as widgets
 from .widget_viewer import Viewer
+from ipydatawidgets import NDArray, array_serialization, shape_constraints
+from traitlets import CBool
+import matplotlib.pyplot as plt
+import matplotlib
+import IPython
 
 @widgets.register
 class LineProfiler(Viewer):
@@ -17,11 +23,26 @@ class LineProfiler(Viewer):
     _model_module = Unicode('itk-jupyter-widgets').tag(sync=True)
     _view_module_version = Unicode('^0.12.2').tag(sync=True)
     _model_module_version = Unicode('^0.12.2').tag(sync=True)
+    point1 = NDArray(dtype=np.float64, default_value=np.zeros((3,), dtype=np.float64),
+                help="First point in physical space that defines the line profile")\
+            .tag(sync=True, **array_serialization)\
+            .valid(shape_constraints(3,))
+    point2 = NDArray(dtype=np.float64, default_value=np.ones((3,), dtype=np.float64),
+                help="First point in physical space that defines the line profile")\
+            .tag(sync=True, **array_serialization)\
+            .valid(shape_constraints(3,))
+    _select_initial_points = CBool(default_value=False, help="We will select the initial points for the line profile.").tag(sync=True)
 
     def __init__(self, **kwargs):
+        if 'point1' not in kwargs or 'point2' not in kwargs:
+            self._select_initial_points = True
+            # Default to z-plane mode instead of the 3D volume if we need to
+            # select points
+            if 'mode' not in kwargs:
+                kwargs['mode'] = 'z'
         super(LineProfiler, self).__init__(**kwargs)
 
-def line_profile(image):
+def line_profile(image, **viewer_kwargs):
     """View the image with a line profile.
 
     Creates and returns an ipywidget to visualize the image along with a line
@@ -34,8 +55,28 @@ def line_profile(image):
     image : array_like, itk.Image, or vtk.vtkImageData
         The 2D or 3D image to visualize.
 
+    viewer_kwargs :
+        Keyword arguments for the viewer. See help(itkwidgets.view).
+
     """
 
-    viewer = LineProfiler(image=image)
+    profiler = LineProfiler(image=image, **viewer_kwargs)
 
-    return viewer
+    ipython = IPython.get_ipython()
+    ipython.enable_matplotlib('widget')
+
+    is_interactive = matplotlib.is_interactive()
+    matplotlib.interactive(False)
+
+    x = np.linspace(0, 2*np.pi, 400)
+    y = np.sin(x**2)
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+    widget = widgets.VBox([profiler, fig.canvas])
+
+    matplotlib.interactive(is_interactive)
+
+    return widget
--- js/lib/lineProfiler.js
+++ js/lib/lineProfiler.js
@@ -1,5 +1,9 @@
 const viewer = require('./viewer')
+const widgets = require('@jupyter-widgets/base')
 const  _ = require('lodash')
+import {
+    fixed_shape_serialization
+} from "jupyter-dataserializers"
 import vtkLineWidget from 'vtk.js/Sources/Interaction/Widgets/LineWidget'
 import macro from 'vtk.js/Sources/macro';
 
@@ -12,8 +16,15 @@ const LineProfilerModel = viewer.ViewerModel.extend({
       _view_module: 'itk-jupyter-widgets',
       _model_module_version: '0.12.2',
       _view_module_version: '0.12.2',
+      point1: new Float64Array([0.0, 0.0, 0.0]),
+      point2: new Float64Array([1.0, 1.0, 1.0]),
+      _select_initial_points: false,
     })
-  }},
+  }}, {
+  serializers: _.extend({
+    point1: fixed_shape_serialization([3,]),
+    point2: fixed_shape_serialization([3,]),
+  }, widgets.DOMWidgetModel.serializers) }
 )
 
 
@@ -25,15 +36,28 @@ const LineProfilerView = viewer.ViewerView.extend({
     this.model.lineWidget = lineWidget
     lineWidget.setInteractor(viewProxy.getInteractor())
     lineWidget.setEnabled(1)
-    lineWidget.setWidgetStateToStart()
+    const selectInitialPoints = this.model.get('_select_initial_points')
+    if (selectInitialPoints) {
+      lineWidget.setWidgetStateToStart()
+    } else {
+      lineWidget.setWidgetStateToManipulate()
+    }
+    this.model.on('change:point1', this.point1_changed, this)
+    this.model.on('change:point2', this.point2_changed, this)
     const volumeRepresentation = viewProxy.getRepresentations()[0]
     const lineRepresentation = lineWidget.getWidgetRep()
+    this.model.lineRepresentation = lineRepresentation
+    const initialPoint1Position = this.model.get('point1')
+    lineRepresentation.setPoint1WorldPosition(Array.from(initialPoint1Position));
+    const initialPoint2Position = this.model.get('point2')
+    lineRepresentation.setPoint2WorldPosition(Array.from(initialPoint2Position));
     const lineActor = lineRepresentation.getActors()[2]
     const lineMapper = lineActor.getMapper()
     const renderWindow = viewProxy.getRenderWindow()
+    this.model.renderWindow = renderWindow
     lineMapper.setRelativeCoincidentTopologyLineOffsetParameters(-4, -4)
     lineMapper.setResolveCoincidentTopology(true)
-    function onInteractionEvent() {
+    const onInteractionEvent = () => {
       const mode = viewProxy.getViewMode()
       const point1Position = lineRepresentation.getPoint1WorldPosition()
       const point2Position = lineRepresentation.getPoint2WorldPosition()
@@ -80,8 +104,9 @@ const LineProfilerView = viewer.ViewerView.extend({
       default:
         vtkErrorMacro('Unexpected view mode');
       }
-      console.log('InteractionEvent!')
-      console.log(lineRepresentation.getPoint1WorldPosition())
+      this.model.set('point1', new Float64Array(point1Position))
+      this.model.set('point2', new Float64Array(point2Position))
+      this.model.save_changes()
     }
     const debouncedOnInteractionEvent = macro.debounce(onInteractionEvent, 200);
     lineWidget.onInteractionEvent(debouncedOnInteractionEvent)
@@ -89,6 +114,23 @@ const LineProfilerView = viewer.ViewerView.extend({
     this.model.itkVtkViewer.subscribeXSliceChanged(debouncedOnInteractionEvent)
     this.model.itkVtkViewer.subscribeYSliceChanged(debouncedOnInteractionEvent)
     this.model.itkVtkViewer.subscribeZSliceChanged(debouncedOnInteractionEvent)
+  },
+
+  point1_changed: function() {
+    const modelPoint1Position = Array.from(this.model.get('point1'))
+    const representationPoint1Position = this.model.lineRepresentation.getPoint1WorldPosition()
+    if (!_.isEqual(modelPoint1Position, representationPoint1Position)) {
+      this.model.lineRepresentation.setPoint1WorldPosition(modelPoint1Position)
+      this.model.renderWindow.render()
+    }
+  },
+  point2_changed: function() {
+    const modelPoint2Position = Array.from(this.model.get('point2'))
+    const representationPoint2Position = this.model.lineRepresentation.getPoint2WorldPosition()
+    if (!_.isEqual(modelPoint2Position, representationPoint2Position)) {
+      this.model.lineRepresentation.setPoint2WorldPosition(modelPoint2Position)
+      this.model.renderWindow.render()
+    }
   },
 
 });
