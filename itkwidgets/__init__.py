10f6822d6323d48878994d3ac7ce4d908558fa48
Matt McCormick <matt.mccormick@kitware.com>
2018-12-11 22:21:43 -0500

    ENH: Add checkerboard widget

--- itkwidgets/__init__.py
+++ itkwidgets/__init__.py
@@ -1,6 +1,7 @@
 from ._version import version_info, __version__
 
 from .widget_viewer import Viewer, view
+from .widget_checkerboard import checkerboard
 from .widget_line_profiler import line_profile
 from . import cm
 
---
+++ itkwidgets/widget_checkerboard.py
@@ -0,0 +1,71 @@
+"""Checkerboard class
+
+Compare two images with a checkerboard pattern. This is particularly useful for
+examining registration results.
+"""
+
+from traitlets import Unicode
+
+import numpy as np
+import ipywidgets as widgets
+from .widget_viewer import Viewer
+from traitlets import CInt
+import itk
+from ._to_itk import to_itk_image
+
+def checkerboard(image1, image2, pattern=3, **viewer_kwargs):
+    """Compare two images with a checkerboard pattern.
+
+    This is particularly useful for examining registration results.
+
+    Parameters
+    ----------
+    image1 : array_like, itk.Image, or vtk.vtkImageData
+        First image to use in the checkerboard.
+
+    image2 : array_like, itk.Image, or vtk.vtkImageData
+        Second image to use in the checkerboard.
+
+    pattern : int, optional, default: 3
+        Size of the checkerboard pattern.
+
+    viewer_kwargs : optional
+        Keyword arguments for the viewer. See help(itkwidgets.view).
+
+    """
+
+    checkerboard_filter = itk.CheckerBoardImageFilter.New(image1, image2)
+
+    dimension = image1.GetImageDimension()
+    checker_pattern = [pattern]*dimension
+    checkerboard_filter.SetCheckerPattern(checker_pattern)
+
+    checkerboard_filter.Update()
+    checkerboard = checkerboard_filter.GetOutput()
+
+    if 'annotations' not in viewer_kwargs:
+        viewer_kwargs['annotations'] = False
+    if 'interpolation' not in viewer_kwargs:
+        viewer_kwargs['interpolation'] = False
+    if 'ui_collapsed' not in viewer_kwargs:
+        viewer_kwargs['ui_collapsed'] = True
+    viewer = Viewer(image=checkerboard, **viewer_kwargs)
+
+    max_size1 = int(min(itk.size(image1)) / 8)
+    max_size1 = max(max_size1, pattern*2)
+    max_size2 = int(min(itk.size(image2)) / 8)
+    max_size2 = max(max_size2, pattern*2)
+    max_size = max(max_size1, max_size2)
+
+    pattern_slider = widgets.IntSlider(value=pattern, min=2, max=max_size,
+            step=1, description='Pattern size:')
+    def update_checkerboard(change):
+        checker_pattern = [change.new]*dimension
+        checkerboard_filter.SetCheckerPattern(checker_pattern)
+        checkerboard_filter.Update()
+        viewer.image = checkerboard_filter.GetOutput()
+    pattern_slider.observe(update_checkerboard, ['value'])
+
+    widget = widgets.VBox([viewer, pattern_slider])
+
+    return widget
