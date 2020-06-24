a1b1aaad85981e3dbb139088fd044d53ee4357fa
Matt McCormick <matt.mccormick@kitware.com>
2020-06-24 16:46:14 -0400

    ENH: Bump itkwidgets to 0.30.0

--- itkwidgets/widget_viewer.py
+++ itkwidgets/widget_viewer.py
@@ -115,8 +115,8 @@ class Viewer(ViewerParent):
     _model_name = Unicode('ViewerModel').tag(sync=True)
     _view_module = Unicode('itkwidgets').tag(sync=True)
     _model_module = Unicode('itkwidgets').tag(sync=True)
-    _view_module_version = Unicode('^0.29.0').tag(sync=True)
-    _model_module_version = Unicode('^0.29.0').tag(sync=True)
+    _view_module_version = Unicode('^0.30.0').tag(sync=True)
+    _model_module_version = Unicode('^0.30.0').tag(sync=True)
     image = ITKImage(
         default_value=None,
         allow_none=True,
