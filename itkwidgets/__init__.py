379da4732b5eb537b2dfdf3b27ca2c1e00b512bf
Matt McCormick <matt.mccormick@kitware.com>
2020-07-28 13:44:13 -0400

    ENH: Add label_statistics widget

--- itkwidgets/__init__.py
+++ itkwidgets/__init__.py
@@ -3,6 +3,7 @@ __all__ = ['version_info', '__version__',
            'checkerboard',
            'compare',
            'line_profile',
+           'label_statistics',
            'cm',
            'lut',
            '_jupyter_nbextension_paths']
@@ -13,6 +14,7 @@ from .widget_viewer import Viewer, view
 from .widget_compare import compare
 from .widget_checkerboard import checkerboard
 from .widget_line_profiler import line_profile
+from .widget_label_statistics import label_statistics
 from . import cm, lut
 
 
