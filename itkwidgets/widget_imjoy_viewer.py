8c70cdc01070bfb736c82dd5e1a7abbc9b59ad89
Brianna Major <brianna.major@kitware.com>
2022-05-19 12:08:56 -0400

    Check if we are running in Colab
    If so we want to use showDialog to prevent issues with displaying the plugin.
    For Jupyter we want createWindow so that the plugin is displayed inline.

--- itkwidgets/widget_imjoy_viewer.py
+++ itkwidgets/widget_imjoy_viewer.py
@@ -1,16 +1,24 @@
 from imjoy import api
+from IPython import get_ipython
 
 
 class ImJoyViewer:
     """ImJoy Viewer widget class."""
     def __init__(self, **kwargs):
+        self.running_in_colab = 'google.colab' in str(get_ipython())
         self.image = kwargs.get('image', None)
 
     async def setup(self):
-        viewer = await api.createWindow(
-            type='itk-vtk-viewer',
-            src='https://kitware.github.io/itk-vtk-viewer/app',
-        )
+        if self.running_in_colab:
+            viewer = await api.showDialog(
+                type='itk-vtk-viewer',
+                src='https://kitware.github.io/itk-vtk-viewer/app',
+            )
+        else:
+            viewer = await api.createWindow(
+                type='itk-vtk-viewer',
+                src='https://kitware.github.io/itk-vtk-viewer/app',
+            )
         await viewer.setImage(self.image)
 
 
