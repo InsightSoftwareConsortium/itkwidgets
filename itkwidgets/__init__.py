df0542a4b7c46daeabe17542c33f98950cf08233
Matt McCormick <matt.mccormick@kitware.com>
2022-05-20 18:29:48 -0400

    ENH: Bump version to 1.0a2

--- itkwidgets/__init__.py
+++ itkwidgets/__init__.py
@@ -1,6 +1,6 @@
 """itkwidgets: Interactive widgets to visualize images, point sets, and 3D geometry on the web."""
 
-__version__ = "1.0a1"
+__version__ = "1.0a2"
 
 from .viewer import Viewer, view
 
