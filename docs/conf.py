364abece77f884d719b5dc6b6d78529a1a7be3a8
Matt McCormick <matt.mccormick@kitware.com>
2022-07-11 16:47:22 -0400

    BUG: RTD debugging

--- docs/conf.py
+++ docs/conf.py
@@ -70,6 +70,13 @@ html_static_path = ['_static',
         '_output']
 
 def jupyterlite_build(app: Sphinx, error):
+    from pprint import pprint
+    pprint('jupyterlite_build')
+    pprint(error)
+    pprint(app)
+    pprint(app.builder)
+    if app.builder:
+        pprint(app.builder.format)
     if error is not None and app.builder and app.builder.format == "html":
         subprocess.check_call(['jupyter', 'lite', 'build', '--config',
             str(jupyterlite_config)], cwd=str(here))
