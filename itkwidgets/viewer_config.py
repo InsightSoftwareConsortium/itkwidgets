7ccfb7f0c5198e56a6f285ff75ab0f3524e5a6af
Matt McCormick <matt.mccormick@kitware.com>
2022-10-03 09:40:21 -0400

    ENH: Update viewer for large robustness, cinematic volume scattering

--- itkwidgets/viewer_config.py
+++ itkwidgets/viewer_config.py
@@ -1,5 +1,5 @@
 ITK_VIEWER_SRC = (
-    "https://bafybeidzxfofr6cwqq6oxsbfwjpfufrf5nuxt56s42kqr6qsjphv5hfpce.on.fleek.co/"
+    "https://bafybeigbvcrknjanx5gvlipuvkcsysjx3i5n7gsmi53s4i6mm37nizzljq.on.fleek.co/"
 )
 PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.11.4/dist/bootstrapUIMachineOptions.js.es.js"
 MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
