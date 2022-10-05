4d16a90a38724c51b108007ac48e68ef5467b23b
Matt McCormick <matt.mccormick@kitware.com>
2022-10-05 14:17:09 -0400

    ENH: Update viewer for volumetric scattering basics

--- itkwidgets/viewer_config.py
+++ itkwidgets/viewer_config.py
@@ -1,5 +1,5 @@
 ITK_VIEWER_SRC = (
-    "https://bafybeigbvcrknjanx5gvlipuvkcsysjx3i5n7gsmi53s4i6mm37nizzljq.on.fleek.co/"
+    "https://bafybeih326hyc6zqkbsfskdu56fbfuutvelk2sycdic74unaggsulheffu.on.fleek.co/"
 )
-PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.11.4/dist/bootstrapUIMachineOptions.js.es.js"
+PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.12.0/dist/bootstrapUIMachineOptions.js.es.js"
 MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
