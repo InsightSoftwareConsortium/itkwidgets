3b2dafd791e81ebd216da0b8ea897304411ef979
Matt McCormick <matt.mccormick@kitware.com>
2022-10-06 11:06:14 -0400

    BUG: Update pydata-sphinx ui for log histogram, ui-scroll fixes

--- itkwidgets/viewer_config.py
+++ itkwidgets/viewer_config.py
@@ -1,5 +1,5 @@
 ITK_VIEWER_SRC = (
     "https://bafybeiervmpz64cha6txkd2fqi7n4fgcuhaqntllccvc5po2aqknefchw4.on.fleek.co/"
 )
-PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.12.0/dist/bootstrapUIMachineOptions.js.es.js"
+PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.12.1/dist/bootstrapUIMachineOptions.js.es.js"
 MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
