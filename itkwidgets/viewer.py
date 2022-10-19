259f2e4d34710aa2aeb718e00374a675954aee8f
Brianna Major <brianna.major@kitware.com>
2022-10-19 12:56:27 -0400

    BUG: Fix functionality in Colab
    When running a notebook in Colab the viewer was failing on to_ngff_zarr. It
    seems that in Colab specifically there are issues using Dask from an
    asynchronous function. By moving this to the (now) synchronous setup function we
    avoid this issue.

--- itkwidgets/viewer.py
+++ itkwidgets/viewer.py
@@ -47,8 +47,23 @@ class ViewerRPC:
                 inputs.append((option, data))
         return inputs
 
-    async def setup(self):
-        pass
+    def setup(self):
+        self.init_data.clear()
+        input_data = self._get_input_data()
+        result= None
+        for (input_type, data) in input_data:
+            render_type = _detect_render_type(data, input_type)
+            key = init_key_aliases()[input_type]
+            if render_type is RenderType.IMAGE:
+                if input_type == 'label_image':
+                    result = _get_viewer_image(data, label=True)
+                else:
+                    result = _get_viewer_image(data, label=False)
+            elif render_type is RenderType.POINT_SET:
+                result = _get_viewer_point_sets(data)
+            if result is None:
+                result = data
+            self.init_data[key] = result
 
     async def run(self, ctx):
         """ImJoy plugin setup function."""
@@ -72,24 +87,6 @@ class ViewerRPC:
             config = ui
         else:
             config = {}
-
-        inputs = self._get_input_data()
-
-        self.init_data.clear()
-        result= None
-        for (input_type, data) in inputs:
-            render_type = _detect_render_type(data, input_type)
-            key = init_key_aliases()[input_type]
-            if render_type is RenderType.IMAGE:
-                if input_type == 'label_image':
-                    result = _get_viewer_image(data, label=True)
-                else:
-                    result = _get_viewer_image(data, label=False)
-            elif render_type is RenderType.POINT_SET:
-                result = _get_viewer_point_sets(data)
-            if result is None:
-                result = data
-            self.init_data[key] = result
 
         itk_viewer = await api.createWindow(
             name=f"itkwidgets viewer {_viewer_count}",
