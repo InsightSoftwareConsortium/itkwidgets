4296fa45837b5cf6323ea7a831ff358e84390a38
Matt McCormick <matt.mccormick@kitware.com>
2022-11-29 15:29:29 -0500

    ENH: Bump itk to 5.3.0
    Adds support for Ptyhon 3.10, 3.11.

--- setup.py
+++ setup.py
@@ -191,10 +191,11 @@ setup_args = {
         'Intended Audience :: Science/Research',
         'Topic :: Multimedia :: Graphics',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 }
 
