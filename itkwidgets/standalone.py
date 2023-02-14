235130d35760d369ebd7ab6dd373d0281628515d
Brianna Major <brianna.major@kitware.com>
2023-02-14 13:30:54 -0500

    ENH: Start hypha server when script is run

--- itkwidgets/standalone.py
+++ itkwidgets/standalone.py
@@ -1,10 +1,11 @@
 import argparse
 import asyncio
+import subprocess
+import sys
 
-async def start_server(server_url):
-    config = { 'server_url': server_url, 'workspace': 'viewer' }
-    server = await connect_to_server(config)
-    print(server)
+
+def start_hypha_server():
+    subprocess.run([sys.executable, "-m", "hypha.server"], capture_output=True)
 
 # async def main():
 #     ws = await connect_to_server(config)
@@ -16,3 +17,5 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, help='path to a data file')
     parser.add_argument('--type', type=str, help='Type of data (image, label_image, point_set)')
     opt = parser.parse_args()
+
+    start_hypha_server()
