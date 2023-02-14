fa9fb251bf1cd275431121e47cf56ee19c0c8adc
Brianna Major <brianna.major@kitware.com>
2023-02-14 13:43:22 -0500

    ENH: Start server and open tab with viewer on script run

--- itkwidgets/standalone.py
+++ itkwidgets/standalone.py
@@ -1,15 +1,41 @@
 import argparse
 import asyncio
+import functools
+import socketio
 import subprocess
 import sys
+import webbrowser
 
+from aiohttp import web
+from imjoy_rpc.hypha import connect_to_server
+from itkwidgets.standalone.config import HYPHA_SERVER_URL, WS_SERVER_URL, WS_PORT
+from pathlib import Path
+
+
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+server = None
+
+this_dir = Path(__file__).resolve().parent
+
+async def index(request):
+    '''Serve the client-side application.'''
+    with open(this_dir / 'standalone' / 'index.html') as f:
+        return web.Response(body=server, text=f.read(), content_type='text/html')
+
+app.router.add_get('/', index)
 
 def start_hypha_server():
     subprocess.run([sys.executable, "-m", "hypha.server"], capture_output=True)
 
-# async def main():
-#     ws = await connect_to_server(config)
-#     print(ws)
+async def start_server(server_url, data, app):
+    await connect_to_server({
+        'client_id': 'py-itkwidgets-server',
+        'name': 'py-itk-vtk-viewer-server',
+        'server_url': server_url,
+    })
+    webbrowser.open_new_tab(WS_SERVER_URL)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -19,3 +45,9 @@ if __name__ == '__main__':
     opt = parser.parse_args()
 
     start_hypha_server()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    app.on_startup.append(functools.partial(start_server, HYPHA_SERVER_URL, opt))
+    web.run_app(app, port=WS_PORT, loop=loop)
