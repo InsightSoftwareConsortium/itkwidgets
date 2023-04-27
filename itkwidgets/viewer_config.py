9d225e3e814e64efba7a36a223c3b912a4689b3a
Paul Elliott <paul.elliott@kitware.com>
2023-04-27 13:20:39 -0400

    BLD: bump itk-vtk-viewer to always show compare swap image button

--- itkwidgets/viewer_config.py
+++ itkwidgets/viewer_config.py
@@ -1,5 +1,5 @@
 ITK_VIEWER_SRC = (
-    "https://bafybeibp3znmogg7kp4kujxioomg2h4jkp6caapwtal4im5jdcjndpj52q.on.fleek.co/"
+    "https://bafybeidvfjibujya47zn63mwfc2m4goasxobz6poyigtclyodmalakvmye.on.fleek.co/"
 )
-PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.21.0/dist/bootstrapUIMachineOptions.js.es.js"
+PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.22.0/dist/bootstrapUIMachineOptions.js.es.js"
 MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
