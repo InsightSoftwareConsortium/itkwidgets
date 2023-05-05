c84d8a5c9a4c9bc1cc8163d2f71634c3b29110a3
Brianna Major <brianna.major@kitware.com>
2023-05-05 08:39:11 -0400

    Bump itk-vtk-viewer to 14.39.0
    Adds the fix that improves the windowing step.
    
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- itkwidgets/viewer_config.py
+++ itkwidgets/viewer_config.py
@@ -1,5 +1,5 @@
 ITK_VIEWER_SRC = (
-    "https://bafybeidvfjibujya47zn63mwfc2m4goasxobz6poyigtclyodmalakvmye.on.fleek.co/"
+    "https://bafybeiae4tz4fpdrwwpodiec2jxvxggolrvw66kjdjevkhj7r5nhlw4kva.on.fleek.co/"
 )
 PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.22.0/dist/bootstrapUIMachineOptions.js.es.js"
 MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
