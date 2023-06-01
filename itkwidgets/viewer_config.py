e3c48f24c39cf1d6719a7e7f46c11208c37e0806
Brianna Major <brianna.major@kitware.com>
2023-06-01 13:35:19 -0400

    ENH: Add bounding box feature
    Allows user to display a bounding box around each of the image layers.

--- itkwidgets/viewer_config.py
+++ itkwidgets/viewer_config.py
@@ -1,5 +1,5 @@
 ITK_VIEWER_SRC = (
-    "https://bafybeibp7aewb5ngv3o3kkqzd523b7742eumq23mllxba5tz2eakqxxwca.on.fleek.co/"
+    "https://bafybeigxr6gde3tvni2wq732hzwwabhodjajxtn4lf7dut5d4zist5wqxe.on.fleek.co/"
 )
-PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.23.0/dist/bootstrapUIMachineOptions.js.es.js"
+PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.24.0/dist/bootstrapUIMachineOptions.js.es.js"
 MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
