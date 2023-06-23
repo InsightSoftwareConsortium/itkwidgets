1cd0c872ad1f47da9201436a5944244591161903
Brianna Major <brianna.major@kitware.com>
2023-06-23 11:40:28 -0400

    ENH: Add remaining command line args
    The command line can now accept all of the same arguments that view() can
    accept.

--- itkwidgets/standalone_server.py
+++ itkwidgets/standalone_server.py
@@ -17,7 +17,11 @@ import itk
 from imjoy_rpc.hypha import connect_to_server
 from itkwidgets.standalone.config import SERVER_HOST, SERVER_PORT, VIEWER_HTML
 from itkwidgets.imjoy import register_itkwasm_imjoy_codecs_cli
-from itkwidgets._initialization_params import build_config, build_init_data
+from itkwidgets._initialization_params import (
+    build_config,
+    build_init_data,
+    init_params_dict
+)
 from pathlib import Path
 from urllib.parse import parse_qs, urlencode, urlparse
 
@@ -57,6 +61,14 @@ def input_dict():
     return { 'data': data }
 
 
+def viewer_ready(itk_viewer):
+    init_viewer_kwargs = vars(opts)
+    settings = init_params_dict(itk_viewer)
+    for key, value in init_viewer_kwargs.items():
+        if key in settings.keys() and value is not None:
+            settings[key](value)
+
+
 async def start_viewer(server_url):
     server = await connect_to_server({
         'client_id': 'itkwidgets-server',
@@ -77,6 +89,18 @@ async def start_viewer(server_url):
             "run_in_executor": False,
         },
         "inputObject": input_dict
+    })
+
+    await server.register_service({
+        "name": "itkwidgets_viewer_ready",
+        "id": "itkwidgets-viewer-ready",
+        "description": "",
+        "config": {
+            "visibility": "protected",
+            "require_context": False,
+            "run_in_executor": False,
+        },
+        "viewerReady": viewer_ready
     })
 
     workspace = server.config.workspace
@@ -125,16 +149,174 @@ def main():
         proc.terminate()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--image', type=str, help='path to an image data file')
-    parser.add_argument('--label-image', type=str, help='path to a label image data file')
-    parser.add_argument('--point-set', type=str, help='path to a point set data file')
-    parser.add_argument('--use2D', dest='use2D', action='store_true', default=False, help='Image is 2D')
-    parser.add_argument('--rotate', dest='rotate', action='store_true', default=False, help='initialize viewer with rotating image')
-    parser.add_argument('--ui', type=str, choices=['reference', 'pydata-sphinx'], default='reference', help='Which UI to use')
-    parser.add_argument('--gradientOpacity', type=float, help='Set the gradient opacity in the volume rendering. Values range from 0.0 to 1.0.')
+    parser.add_argument("data", nargs="?", type=str, help="Path to a data file.")
+    parser.add_argument("--image", type=str, help="Path to an image data file.")
+    parser.add_argument(
+        "--label-image", type=str, help="Path to a label image data file."
+    )
+    parser.add_argument("--point-set", type=str, help="Path to a point set data file.")
+    parser.add_argument(
+        "--use2D", dest="use2D", action="store_true", default=False, help="Image is 2D."
+    )
+    # General Interface
+    parser.add_argument(
+        "--rotate",
+        dest="rotate",
+        action="store_true",
+        default=False,
+        help="Continuously rotate the camera around the scene in volume rendering mode.",
+    )
+    parser.add_argument(
+        "--ui",
+        type=str,
+        choices=["reference", "pydata-sphinx"],
+        default="reference",
+        help="Which UI to use",
+    )
+    parser.add_argument(
+        "--ui-collapsed",
+        dest="ui_collapsed",
+        action="store_true",
+        default=False,
+        help="Collapse the native widget user interface.",
+    )
+    parser.add_argument(
+        "--no-annotations",
+        dest="annotations",
+        action="store_false",
+        default=True,
+        help="Display annotations describing orientation and the value of a mouse-position-based data probe.",
+    )
+    parser.add_argument(
+        "--axes", dest="axes", action="store_true", default=False, help="Display axes."
+    )
+    parser.add_argument(
+        "--bg-color",
+        type=tuple,
+        nargs='+',
+        help="Background color: (red, green, blue) tuple, components from 0.0 to 1.0.",
+    )
+    # Images
+    parser.add_argument(
+        "--label-blend",
+        type=float,
+        help="Label map blend with intensity image, from 0.0 to 1.0.",
+    )
+    parser.add_argument(
+        "--label-names",
+        type=list,
+        nargs='+',
+        help="String names associated with the integer label values. List of (label_value, label_name).",
+    )
+    parser.add_argument(
+        "--label-lut",
+        type=str,
+        help="Lookup table for the label map.",
+    )
+    parser.add_argument(
+        "--label-weights",
+        type=float,
+        help="The rendering weight assigned to current label. Values range from 0.0 to 1.0.",
+    )
+    parser.add_argument(
+        "--color-range",
+        type=list,
+        nargs='+',
+        help="The [min, max] range of intensity values mapped to colors for the given image component identified by name.",
+    )
+    parser.add_argument(
+        "--color-bounds",
+        type=list,
+        nargs='+',
+        help="The [min, max] range of intensity values for color maps that provide a bounds for user inputs.",
+    )
+    parser.add_argument(
+        "--cmap",
+        type=str,
+        help="The color map for the current component/channel.",
+    )
+    parser.add_argument(
+        "--x-slice",
+        type=float,
+        help="The position in world space of the X slicing plane.",
+    )
+    parser.add_argument(
+        "--y-slice",
+        type=float,
+        help="The position in world space of the Y slicing plane.",
+    )
+    parser.add_argument(
+        "--z-slice",
+        type=float,
+        help="The position in world space of the Z slicing plane.",
+    )
+    parser.add_argument(
+        "--no-interpolation",
+        dest="interpolation",
+        action="store_false",
+        default=True,
+        help="Linear as opposed to nearest neighbor interpolation for image slices.",
+    )
+    parser.add_argument(
+        "--gradient-opacity",
+        type=float,
+        help="Gradient opacity for composite volume rendering, in the range (0.0, 1.0].",
+    )
+    parser.add_argument(
+        "--gradient-opacity-scale",
+        type=float,
+        help="Gradient opacity scale for composite volume rendering, in the range (0.0, 1.0].",
+    )
+    parser.add_argument(
+        "--blend-mode",
+        type=str,
+        help='Volume rendering blend mode. Supported modes: "Composite", "Maximum", "Minimum", "Average".',
+    )
+    parser.add_argument(
+        "--component-hidden",
+        dest="component_visible",
+        action="store_false",
+        default=True,
+        help="Whether to used gradient-based shadows in the volume rendering.",
+    )
+    parser.add_argument(
+        "--shadow-disabled",
+        dest="shadow_enabled",
+        action="store_false",
+        default=True,
+        help="Whether to used gradient-based shadows in the volume rendering.",
+    )
+    parser.add_argument(
+        "--view-mode",
+        type=str,
+        choices=["XPlane", "YPlane", "ZPlane", "Volume"],
+        help="Only relevant for 3D scenes.",
+    )
+    parser.add_argument(
+        "--layer",
+        type=str,
+        help="Select the layer identified by `name` in the user interface.",
+    )
+    parser.add_argument(
+        "--layer-hidden",
+        dest="layer_visible",
+        action="store_false",
+        default=True,
+        help="Whether the current layer is visible.",
+    )
+    # Other Parameters
+    parser.add_argument(
+        "--sample-distance",
+        type=float,
+        help="Sampling distance for volume rendering, normalized from 0.0 to 1.0. Lower values result in a higher quality rendering. High values improve the framerate.",
+    )
+    parser.add_argument(
+        "--units", type=str, help="Units to display in the scale bar."
+    )
+
     opts = parser.parse_args()
 
     main()
