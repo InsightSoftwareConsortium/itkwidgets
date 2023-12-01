22d1fe4e2ccadf59c69ceb02f418f580e24a7179
Matt McCormick <matt.mccormick@kitware.com>
2023-12-01 06:48:01 -0500

    DOC: Switch to Furo theme
    Consistent with docs.itk.org, wasm.itk.org.
    
    Favicon generated with https://realfavicongenerator.net/

--- docs/conf.py
+++ docs/conf.py
@@ -18,12 +18,14 @@ from sphinx.application import Sphinx
 import subprocess
 import os
 import re
+from datetime import date
 
 # -- Project information -----------------------------------------------------
 
 project = 'itkwidgets'
-copyright = '2023, NumFOCUS'
 author = 'Matthew McCormick'
+copyright = f'{date.today().year}, NumFOCUS'
+author = 'Insight Software Consortium'
 
 # The full version, including alpha/beta/rc tags.
 release = re.sub('^v', '', os.popen('git tag --list "v1.0*" --sort=creatordate').readlines()[-1].strip())
@@ -36,7 +38,27 @@ version = release
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser',]
+extensions = [
+    'myst_parser',
+    'sphinx_copybutton',
+    'sphinx.ext.intersphinx',
+    'sphinxext.opengraph',
+    'sphinx_design',
+]
+
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",  # Support syntax for inline and block math using `$...$` and `$$...$$`
+                   # (see https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#dollar-delimited-math)
+    "fieldlist",
+    "linkify",  # convert bare links to hyperlinks
+]
+
+intersphinx_mapping = {
+    "itkwasm": ("https://wasm.itk.org/en/latest/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+}
 
 html_theme_options = dict(
     github_url='https://github.com/InsightSoftwareConsortium/itkwidgets',
@@ -59,7 +81,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'furo'
+html_logo = "_static/itkwidgets_logo_small.png"
+html_favicon = "_static/favicon/favicon.ico"
+html_title = f"{project}'s documentation"
+
+# Furo options
+html_theme_options = {
+    "top_of_page_button": "edit",
+    "source_repository": "https://github.com/InsightSoftwareConsortium/itkwidgets/",
+    "source_branch": "main",
+    "source_directory": "docs",
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
