f81c03744da454f7689ff2022103b018e3366061
Brianna Major <brianna.major@kitware.com>
2023-12-07 10:34:38 -0500

    BUILD: Bump itk-vtk-viewer version to 14.45.0

--- itkwidgets/viewer_config.py
+++ itkwidgets/viewer_config.py
@@ -1,5 +1,5 @@
 ITK_VIEWER_SRC = (
-    "https://bafybeiauuswm657tct7b7kpnorrsz7hvvaljpsbeuw35hkfobtyyrbblpm.on.fleek.co/"
+    "https://bafybeiasdu4jg6lbpttmtjoj5zujy7mbqao4gdedvq7jftgqmawi75mcaq.on.fleek.co/"
 )
 PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.26.2/dist/bootstrapUIMachineOptions.js.es.js"
 MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
